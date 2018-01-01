5c4b5f3a58d5318022af43fde3cf7ba87c952a25
Ileana <argyris.ileana@gmail.com>
2017-12-31 21:07:22 -1000

    Finished list1 exercises

--- basic/list1.py
+++ basic/list1.py
@@ -22,7 +22,11 @@
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
   # +++your code here+++
-  return
+	s = 0
+	for i in words:
+		if len(i) > 1 and i.endswith(i[0]):
+				s+= 1
+	return s
 
 
 # B. front_x
@@ -34,7 +38,15 @@ def match_ends(words):
 # before combining them.
 def front_x(words):
   # +++your code here+++
-  return
+	begin = []
+	end = []
+	for i in words:
+		if i.startswith('x'):
+			begin.append(i)
+		else:
+			end.append(i)
+	return sorted(begin) + sorted(end)
+
 
 
 
@@ -44,9 +56,14 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+def MyFun(s):
+	return s[-1]
+
 def sort_last(tuples):
   # +++your code here+++
-  return
+	return sorted(tuples, key=MyFun)
+
+
 
 
 # Simple provided test() function used in main() to print
@@ -75,7 +92,7 @@ def main():
   test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
+
   print
   print 'sort_last'
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
